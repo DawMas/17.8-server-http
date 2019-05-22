e10f3359756b0a2b26c10f56295b8cd52d47f436
Dawid Maslankiewicz <maslankiewiczd@gmail.com>
2019-05-22 18:02:08 +0200

    Change index.js

--- index.js
+++ index.js
@@ -7,22 +7,27 @@ var server = http.createServer();
 server.on('request', function (request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
 
-    fs.readFile('./index.html', 'utf-8',function(err, data) {
-        console.log(data);
-        
 
-        if (request.method === 'GET' && request.url === '/') {
+
+
+    if (request.method === 'GET' && request.url === '/') {
+
+        fs.readFile('./index.html', 'utf-8', function (err, data) {
+            console.log(data);
+
             response.write(data);
-                response.end();
-        } else {
-                response.statusCode = 404;
-                response.write('<img src="https://cdn.windowsreport.com/wp-content/uploads/2019/02/Ddkmd.sys-blue-screen-errors-in-Windows.jpg">');
-                response.end();
+            response.end();
         }
-    })
+        )
+    } else {
+        response.statusCode = 404;
+        response.write('<img src="https://cdn.windowsreport.com/wp-content/uploads/2019/02/Ddkmd.sys-blue-screen-errors-in-Windows.jpg">');
+        response.end();
+    }
+})
 
    
-});
+
 
 server.listen(9000);
 
