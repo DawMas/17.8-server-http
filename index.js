8f0eaeb4d8f43eaf318f88e93f5ec198158cf529
Dawid Maslankiewicz <maslankiewiczd@gmail.com>
2019-05-22 22:40:16 +0200

    Deleted empty spaces in code

--- index.js
+++ index.js
@@ -2,32 +2,23 @@ var fs = require('fs');
 var http = require('http')
 
 
-
 var server = http.createServer();
 server.on('request', function (request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
-
-
-
-
     if (request.method === 'GET' && request.url === '/') {
-
         fs.readFile('./index.html', 'utf-8', function (err, data) {
             console.log(data);
-
             response.write(data);
             response.end();
         }
         )
-    } else {
+    }
+    else {
         response.statusCode = 404;
         response.write('<img src="https://cdn.windowsreport.com/wp-content/uploads/2019/02/Ddkmd.sys-blue-screen-errors-in-Windows.jpg">');
         response.end();
     }
 })
-
-   
-
 
 server.listen(9000);
 
